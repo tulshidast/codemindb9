7f4255d0c8d7feeb87f4a34f1e00af69197706ef
tulshidast <tulsidast77@gmail.com>
2024-01-27 07:34:36 +0530

    screenshots related code added

--- src/test/java/testcases/BaseTest.java
+++ src/test/java/testcases/BaseTest.java
@@ -1,11 +1,18 @@
 package testcases;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 /**
  * 
  * This is as test case related class.
  * 
  */
 import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -23,7 +30,17 @@ public class BaseTest {
 	}
 
 	@AfterMethod(alwaysRun = true)
-	public void tearDown() {
+	public void tearDown(ITestResult res) {
+		if (res.getStatus() == ITestResult.FAILURE) {
+			TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+			File file = takesScreenshot.getScreenshotAs(OutputType.FILE);
+			try {
+				FileUtils.copyFile(file, new File("src/test/resources/screenshots/" + res.getName() + ".png"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		driver.quit();
 	}
 
